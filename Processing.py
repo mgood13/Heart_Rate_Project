7cdb76bcc28d85aaa98d0a282d4832d87f7607b8
Michael Good <Michael@Michaels-MacBook-Pro-2.local>
2018-10-23 01:34:44 -0400

    Broke both programs back down into modules

--- Processing.py
+++ Processing.py
@@ -1,5 +1,7 @@
 import csv
 from Reader import fileReader
+import numpy as np
+
 
 def fileProcessor():
     inputdictionary = fileReader()
@@ -11,6 +13,7 @@ def fileProcessor():
     diffmax = 0
     threshold = 0
     beat_time = []
+    metrics = {}
 
 #Determines the number of files that are expected
     total = inputdictionary.pop('FileNumber')
@@ -18,43 +21,82 @@ def fileProcessor():
 #Loops through each of the files
     for i in inputdictionary:
         with open(i, 'r') as csv1:
-            dialect = inputdictionary.get(i)
-            reader = csv.reader(csv1, dialect)
 
-            #Populates the lists for time and voltage
-            for row in reader:
-                timelist.append(float(row[0]))
-                voltagelist.append(float(row[1]))
-                count += 1
-        #Determines the Max and Min voltages
+            timelist, voltagelist = fileParser(i,csv1, inputdictionary)
+
         timelen = len(timelist)
-        maxvolt = max(voltagelist)
-        minvolt = min(voltagelist)
+        minvolt, maxvolt, duration = ECGMathCalc(timelist, voltagelist, timelen)
 
-        #Determines duration of input signal
-        duration = timelist[timelen-1] - timelist[0]
-        print(duration)
+        diff_vec = differentiator(timelen, voltagelist, timelist)
 
-        #Takes the derivative of the signal for the threshold determination
-
-        for x in range(0,timelen):
-            if x < (timelen -1):
-                diff_vec.append( (voltagelist[x+1] - voltagelist[x]) / (timelist[x+1] - timelist[x]))
 
         diffmax = max(diff_vec)
         threshold = diffmax * scaling
 
-        #Performs the threshold determination and stores the times of the beats
-        count = 0
-        for n in range(0, (timelen-1)):
-            if x > 1:
-                if (diff_vec[n] > threshold and diff_vec[n - 1] < threshold and diff_vec[n-2] < threshold):
-                    count = count + 1
-                    beat_time.append(timelist[n])
+        beatcount, beat_time = beatCounter(timelen, diff_vec, threshold, timelist)
 
 
-    print(count)
-    print(beat_time)
+        metrics['voltage_extremes'] = (minvolt, maxvolt)
+        metrics['duration'] = duration
+        metrics['num_beats'] = beatcount
+        beat_time_array = np.array(beat_time)
+        metrics['beats'] = beat_time_array
+
+    return outputdictionary
+
+def fileParser(i, csv1, inputdictionary):
+    timelist = []
+    voltagelist = []
+    count = 0
+
+    dialect = inputdictionary.get(i)
+    reader = csv.reader(csv1, dialect)
+
+    # Populates the lists for time and voltage
+    for row in reader:
+        timelist.append(float(row[0]))
+        voltagelist.append(float(row[1]))
+        count += 1
+
+    return timelist, voltagelist
+
+
+def ECGMathCalc(timelist, voltagelist, timelen):
+    duration = 0
+
+    #Determines Min and Max voltages in the file
+    minvolt = min(voltagelist)
+    maxvolt = max(voltagelist)
+
+
+    # Determines duration of input signal
+    duration = timelist[timelen-1] - timelist[0]
+    print(duration)
+    return minvolt, maxvolt, duration
+
+def differentiator(timelen, voltagelist, timelist):
+    # Takes the derivative of the signal for the threshold determination
+    diff_vec = []
+
+    for x in range(0, timelen):
+        if x < (timelen - 1):
+            diff_vec.append((voltagelist[x + 1] - voltagelist[x]) / (timelist[x + 1] - timelist[x]))
+
+    return diff_vec
+
+def beatCounter(timelen, diff_vec, threshold, timelist):
+    beatcount = 0
+    beat_time = []
+    for n in range(0, (timelen - 1)):
+        if n > 1:
+            if (diff_vec[n] > threshold and diff_vec[n - 1] < threshold and diff_vec[n-2] < threshold):
+                beatcount += 1
+                beat_time.append(timelist[n])
+
+    return beatcount, beat_time
+
+
 
 if __name__ == "__main__":
     result = fileProcessor()
+    print(result)
--- Reader.py
+++ Reader.py
@@ -3,16 +3,26 @@ import os
 
 
 def fileReader():
-    errornum = 0
     outdictionary = {}
-    templist = []
-    count = 0
 
+    files = csvFinder()
+
+    outdictionary = csvChecker(files)
+    return outdictionary
+
+def csvFinder():
+    # This method finds all files that end in .csv
     files = [f for f in os.listdir('.') if f.endswith(".csv")]
+    return files
+
+def csvChecker(files):
+    # This method checks all .csv files to see if they are valid csv files
+    outdictionary = {}
+    count = 0
     for f in files:
         with open(f, 'r') as csv1:
             errornum = 0
-
+            # Attempt to determine the delimiter type
             try:
                 dialect = csv.Sniffer().sniff(csv1.read(), delimiters=',')
             except csv.Error:
@@ -22,10 +32,12 @@ def fileReader():
             if errornum == 0:
                 outdictionary[f] = dialect
                 count += 1
-
-
     outdictionary['FileNumber'] = count
+    # Return dictionary with file names, delimiter types, and number of valid files
     return outdictionary
+
+
+
 
 
 if __name__ == "__main__":
