087ab4c828ea548d4737af4ec890d9d81648f2b4
Michael Good <Michael@Michaels-MacBook-Pro-2.local>
2018-10-22 21:35:18 -0400

    Added file reading, max, min, and duration calculation

--- Processing.py
+++ Processing.py
@@ -4,18 +4,30 @@ from Reader import fileReader
 def fileProcessor():
     inputdictionary = fileReader()
     count = 0
+    timelist = []
+    voltagelist = []
 
     total = inputdictionary.pop('FileNumber')
 
 
     for i in inputdictionary:
-        count += 1
+        with open(i, 'r') as csv1:
+            dialect = inputdictionary.get(i)
+            reader = csv.reader(csv1, dialect)
+            for row in reader:
+                timelist.append(float(row[0]))
+                voltagelist.append(float(row[1]))
+                count += 1
 
+        timelen = len(timelist)
+        maxvolt = max(voltagelist)
+        minvolt = min(voltagelist)
 
+        print(maxvolt)
+        print(minvolt)
 
-
-
-
+        duration = timelist[timelen-1] - timelist[0]
+        print(duration)
 
 if __name__ == "__main__":
     result = fileProcessor()
