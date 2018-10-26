65280f3cc01cca51e7160b116de63ea3b844cf21
Michael Good <Michael@Michaels-MacBook-Pro-2.local>
2018-10-25 20:07:36 -0400

    More PEP8 formatting...

--- Processing.py
+++ Processing.py
@@ -86,8 +86,8 @@ def differentiator(timelen, voltagelist, timelist):
 
     for x in range(0, timelen):
         if x < (timelen - 1):
-            diff_vec.append((voltagelist[x + 1] - voltagelist[x])
-                            / (timelist[x + 1] - timelist[x]))
+            diff_vec.append((voltagelist[x + 1] - voltagelist[x]) /
+                            (timelist[x + 1] - timelist[x]))
 
     return diff_vec
 
